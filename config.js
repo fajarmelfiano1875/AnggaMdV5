da1185554dbb77e7892d71af50d0d0e6f9dd1c3a
FajarMelfiano <melfin795@gmail.com>
2022-12-09 21:03:34 +0700

    addad

--- config.js
+++ config.js
@@ -14,32 +14,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6281997496643'] //ur owner number
-global.ownername = "𝗔𝗻𝗴𝗴𝗮𝗫𝘆𝘇𝘇" //ur owner name
-global.developer = "𝗔𝗻𝗴𝗴𝗮𝗫𝘆𝘇𝘇" //nama lu
-global.ytname = "YT: 𝗔𝗻𝗴𝗴𝗮𝗫𝘆𝘇𝘇" //ur yt chanel name
+global.vcardowner = ['6285748839210'] //ur owner number
+global.ownername = "Melfiano" //ur owner name
+global.developer = "Melfiano" //nama lu
+global.ytname = "YT: Melfiano" //ur yt chanel name
 global.socialm = "https://github.com/AnggaXyzZ" //ur github or insta name
 global.sgc = "https://chat.whatsapp.com/E3l5YxNNUx89ecCRpOoOtO"
 global.location = "Banten" //ur location
 
 //bot bomdy 
-global.owner = ['6281997496643'] //ur number
-global.ownernomer = "6281997496643" //ur number
-global.ownertag = '6281997496643' //ur tag number
-global.premium = ['6281997496643'] //ur premium number
-global.botname = '𝗔𝗻𝗴𝗴𝗮𝗕𝗼𝘁𝘇' //ur bot name
-global.ownername = "𝗔𝗻𝗴𝗴𝗮𝗫𝘆𝘇𝘇"
+global.owner = ['6285748839210'] //ur number
+global.ownernomer = "6285748839210" //ur number
+global.ownertag = '6285748839210' //ur tag number
+global.premium = ['6285748839210'] //ur premium number
+global.botname = 'MT-BOTZ' //ur bot name
+global.ownername = "Melfiano"
 global.linkz = "https://youtube.com/@AnggaXyzz" //your theme url which will be displayed on whatsapp
-global.dana = "6281997496643"
-global.gopay = "6281997496643"
-global.pulsa = "6281997496643"
+global.dana = "6285748839210"
+global.gopay = "6285748839210"
+global.pulsa = "6285748839210"
 global.websitex = "https://youtube.com/@AnggaXyzz" //ur website to be displayed
 global.websitex1 = "https://youtube.com/shorts/N6cPd_V-IME?feature=share"
 global.botscript = 'https://github.com/AnggaXyzZ' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "𝗔𝗻𝗴𝗴𝗮𝗕𝗼𝘁𝘇" //ur sticker watermark author
-global.wm = "𝗔𝗻𝗴𝗴𝗮𝗫𝘆𝘇𝘇" //ur watermark
+global.author = "MT-BOTZ" //ur sticker watermark author
+global.wm = "Melfiano" //ur watermark
 
 // Other
 global.sessionName = 'Session'
